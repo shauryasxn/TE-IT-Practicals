cec23ff43efd195b96874d60bf948c992cf67dee
shauryasxn <shaurya1003@gmail.com>
2023-11-20 16:58:30 +0530

    Updated Code

--- DAA/tsp.cpp
+++ DAA/tsp.cpp
@@ -6,43 +6,34 @@ const int MAX = 10;
 int ary[MAX][MAX], completed[MAX], n, cost = 0;
 
 void takeInput() {
-    int i, j;
-
     cout << "Enter the number of villages: ";
     cin >> n;
 
     cout << "\nEnter the Cost Matrix\n";
-
-    for (i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++) {
         cout << "\nEnter Elements of Row: " << i + 1 << "\n";
-
-        for (j = 0; j < n; j++)
+        for (int j = 0; j < n; j++)
             cin >> ary[i][j];
-
         completed[i] = 0;
     }
 
     cout << "\n\nThe cost list is:";
-
-    for (i = 0; i < n; i++) {
+    for (int i = 0; i < n; i++) {
         cout << "\n";
-
-        for (j = 0; j < n; j++)
+        for (int j = 0; j < n; j++)
             cout << "\t" << ary[i][j];
     }
 }
 
 int least(int c) {
-    int i, ncity = 999;
-    int min = 999, kmin;
+    int ncity = 999, min = 999, kmin;
 
-    for (i = 0; i < n; i++) {
-        if ((ary[c][i] != 0) && (completed[i] == 0))
-            if (ary[c][i] + ary[i][c] < min) {
-                min = ary[i][0] + ary[c][i];
-                kmin = ary[c][i];
-                ncity = i;
-            }
+    for (int i = 0; i < n; i++) {
+        if (ary[c][i] != 0 && completed[i] == 0 && ary[c][i] + ary[i][c] < min) {
+            min = ary[i][0] + ary[c][i];
+            kmin = ary[c][i];
+            ncity = i;
+        }
     }
 
     if (min != 999)
@@ -63,7 +54,6 @@ void mincost(int city) {
         ncity = 0;
         cout << ncity + 1;
         cost += ary[city][ncity];
-
         return;
     }
 
@@ -74,7 +64,7 @@ int main() {
     takeInput();
 
     cout << "\n\nThe Path is:\n";
-    mincost(0); // passing 0 because starting vertex
+    mincost(0);
 
     cout << "\n\nMinimum cost is " << cost;
 
