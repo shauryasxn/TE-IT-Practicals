7bce821b150e54b14769df7b4c7243e9a3d63c37
shauryasxn <shaurya1003@gmail.com>
2023-11-20 13:27:06 +0530

    Update

--- DAA/fracknapsack.cpp
+++ DAA/fracknapsack.cpp
@@ -3,8 +3,8 @@
 using namespace std;
 
 struct item {
-    int weight;
-    double ratio, profit;
+    int weight, profit;
+    double ratio;
 };
 
 bool compare (item a, item b) {
@@ -12,20 +12,20 @@ bool compare (item a, item b) {
 }
 
 float fracKnapsack (vector<item> items , int capacity, int n) {
-    double profit = 0;
+    double finalprofit = 0;
 
     for (int i=0; i<n; i++) {
         if (items[i].weight<= capacity) {
             capacity -= items[i].weight;
-            profit += items[i].profit;
+            finalprofit += items[i].profit;
         }
         else {
             double fraction = (double) capacity / items[i].weight;
-            profit += fraction * items[i].profit;
+            finalprofit += (double) fraction * items[i].profit;
             break;
         }
     }
-    return profit;
+    return finalprofit;
 }
 
 int main() {
@@ -39,9 +39,10 @@ int main() {
     for (int i=0; i<n; i++) {
         cout << "Enter Profit and Weight of item " << i+1 << ":";
         cin >> items[i].profit >> items[i].weight;
+        items[i].ratio = items[i].profit/items[i].weight;
         }
     sort(items.begin(), items.end(), compare);
-    float profit = fracKnapsack(items, capacity, n);
-    cout << endl << "Maximum possible profit is: " << profit;
+    float finalprofit = fracKnapsack(items, capacity, n);
+    cout << endl << "Maximum possible profit is: " << finalprofit;
     return 0;
 }
