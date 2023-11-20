8842c0ca22249b7871024d7871e31bd301384620
shauryasxn <shaurya1003@gmail.com>
2023-11-20 18:03:39 +0530

    Updated Code

--- DAA/nqueen.cpp
+++ DAA/nqueen.cpp
@@ -1,56 +1,55 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
+
+bool foundSolution = false;  
 
 void print(int n, int col[]) {
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < n; j++)
-            cout << (col[i] == j ? i + 1 : 0) << " ";
+            cout << (col[i] == j ? 'Q' : '.') << " ";
         cout << "\n";
     }
     cout << "\n";
 }
 
 bool place(int queen, int c, int col[]) {
-    int j = 0;
-    while (j < queen)
+    for (int j = 0; j < queen; j++)
         if (col[j] == c || abs(col[j] - c) == abs(j - queen))
             return false;
-        else
-            j++;
     return true;
 }
 
-bool nqueen(int queen, int n, int col[]) {
-    int fl = 0;
-    for (int i = 0; i < n; i++)
-        if (place(queen, i, col)) {
+void nqueen(int queen, int n, int col[]) {
+    if (queen == n) {
+        print(n, col);
+        foundSolution = true;  
+        return;
+    }
+
+    for (int i = 0; i < n; i++) {
+        if (!foundSolution && place(queen, i, col)) {
             col[queen] = i;
-            cout << "Placing Queen " << queen + 1 << "\n";
-            print(n, col);
-            if (queen == n - 1)
-                return true;
-            if (nqueen(queen + 1, n, col))
-                return true;
-            col[queen] = -1;
-            cout << "Backtracking Queen " << queen + 1 << "\n";
-            print(n, col);
-            fl = 1;
+            nqueen(queen + 1, n, col);
         }
-    if (!fl)
-        cout << "Unable to place Queen " << queen + 1 << "\n\n";
-    return false;
+    }
 }
 
 int main() {
     int n;
     cout << "Enter Size of chessboard : ";
     cin >> n;
+
     if (n <= 3) {
         cout << "\nSize should be more than 3\n";
         return 0;
     }
+
     int col[n];
-    memset(col, -1, sizeof(col));
     nqueen(0, n, col);
+
+    if (!foundSolution) {
+        cout << "No solution found.\n";
+    }
+
     return 0;
 }
